75a6fd83065e5dc90414ea23816d5efad9d02af2
Ertugrul Söylemez <esz@posteo.de>
2016-08-21 04:57:36 +0200

    Skeleton initial revision.

---
+++ programs/skeleton.hs
@@ -0,0 +1,25 @@
+-- Copyright 2016 Ertugrul Söylemez
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- |
+-- Module:     Main
+-- Copyright:  Copyright 2016 Ertugrul Soeylemez
+-- License:    Apache License 2.0
+-- Maintainer: Ertugrul Soeylemez <esz@posteo.de>
+
+module Main where
+
+
+main :: IO ()
+main = pure ()
